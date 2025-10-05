62df9bc0173663c9c8ce73b20093a246f0b23d48
Methat-i <ice.methat@gmail.com>
2025-10-05 15:00:00 +0700

    Add

---
+++ src/features/Product/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+function Product({ item }) {
+  let productImage = '';//ป้องกัน er เถ้าไม่มีภาพ
+
+  if (item.imageURL) {//ถ้ามีค่ารันต่อ
+    if (item.imageURL.startsWith('http') || item.imageURL.startsWith('https')) { //
+      productImage = item.imageURL; // ใช้ URL ภายนอกตรงๆ 
+      //https://placehold.co/200x200/cccccc/ffffff/png
+    } else {
+      try {
+        productImage = require(`../../assets/${item.imageURL}`);
+        //watch.jpg
+      } catch (err) {
+        console.warn(`Image not found: ${item.imageURL}`, err);
+        productImage = require('../../assets/default.jpg');
+      }
+    }
+  } else {
+    productImage = 'https://via.placeholder.com/100';
+  }
+  return (
+    <li className="Products">
+      <a href={`/update-product/${item.id}`}>
+        <img className="Products__image" src={productImage} alt={item.name} />
+        <div className="Products__name">{item.name}</div>
+        <small className="Products__type">{item.type}</small>
+      </a>
+    </li>
+  );
+}
+
+Product.propTypes = {
+  item: PropTypes.object.isRequired
+};
+
+export default Product;
